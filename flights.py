669017b7a85527dc5d7e7988c455d4b2be543edc
AndrewZakri <zakria@sunypoly.edu>
2025-06-20 15:42:12 -0400

    Update flights.py

--- flights.py
+++ flights.py
@@ -9,6 +9,10 @@ import matplotlib.pyplot as plt
 from shapely.geometry import Point, LineString, Polygon
 import streamlit as st
 
+#Title of the app
+st.title("Airline A Dashboard")
+st.write("Analysis of Airline A data to determine how to upgrade the existing fleet")
+
 #Data reading
 
 df = pd.read_csv("Airports_P 1.csv")
@@ -20,6 +24,7 @@ monthly_passengers['Fly_date'] = pd.to_datetime(monthly_passengers['Fly_date'])
 monthly_passengers = monthly_passengers.sort_values(by='Fly_date')
 monthly_passengers['Rolling_Avg'] = monthly_passengers['Passengers'].rolling(window=3).mean()
 
+#Create a line chart
 fig = px.line(
     monthly_passengers,
     x='Fly_date',
@@ -33,6 +38,4 @@ fig = px.line(
 fig.show(0)
 
 # Display the plot in Streamlit
-st.title("Airline Passenger Data")
-st.write("Here is the trend of monthly passengers with a 3-month rolling average.")
 st.plotly_chart(fig)
